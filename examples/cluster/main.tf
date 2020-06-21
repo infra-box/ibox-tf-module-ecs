7026b9b28d0dd8bfde780500b2352bd1d95007a4
Mauricio Hernandez <mauriciohernandez@MacBook-Pro.local>
2020-06-21 16:54:13 -0500

    cluster without replication

--- examples/cluster/main.tf
+++ examples/cluster/main.tf
@@ -1,7 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "maando-shared"
+}
+
 module "ecs_cluster" {
   source = "../../modules/cluster"
 
-  provider_profile = ""
   cluster_name = "test"
   environment = "dev"
 }
--- modules/cluster/outputs.tf
+++ modules/cluster/outputs.tf
@@ -5,13 +5,3 @@ output "cluster_id" {
 output "cluster_name" {
   value = aws_ecs_cluster.default.name
 }
-
-##### replication
-
-output "replica_cluster_id" {
-  value = aws_ecs_cluster.default_replica.*.id
-}
-
-output "replica_cluster_name" {
-  value = aws_ecs_cluster.default_replica.*.name
-}
